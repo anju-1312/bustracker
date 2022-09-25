2224a382e0e70aeb77af74f28fb001eef579ba44
anju-1312 <113588224+anju-1312@users.noreply.github.com>
2022-09-25 23:53:01 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -15,7 +15,8 @@ $conn=new mysqli($servername,$username,$password,$db);
 if ($conn->connect_error) {
 	die("connection failed".$conn->connect_error);
 	# code...
-}else{
+}
+else{
     echo "connected successfully";
     
 }

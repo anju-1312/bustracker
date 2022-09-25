532a0dc55a62d552ef4ca40f2852230dd5d7310d
anju-1312 <113588224+anju-1312@users.noreply.github.com>
2022-09-25 23:18:08 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -6,12 +6,14 @@ $username='Divya';
 $password='Devesh-2002';
 $db='bustracker';
 
+
 $conn=new mysqli($servername,$username,$password,$db);
 if ($conn->connect_error) {
 	die("connection failed".$conn->connect_error);
 	# code...
 }else{
     echo "connected successfully";
+    Log.d("connection is established")
 }
      </defaultProxy>
 </system.net>

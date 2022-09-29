80871930c28e5b452eb513d9d0da90d308104294
anju-1312 <113588224+anju-1312@users.noreply.github.com>
2022-09-29 13:43:29 +0530

    Update checkBus.php

--- checkBus.php
+++ checkBus.php
@@ -6,12 +6,23 @@ $i="not";
 $query="SELECT Source,Destination FROM [dbo].[BusCheck] where Busno='$Bno'";
 $exe=mysqli_query($conn,$query);
 //print_r($exe);
+ $tsql= "SELECT TOP (1000) * FROM [dbo].[bus]";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['Name'] . " " . $row['Bno'] . PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults);
 if ($exe->num_rows>0) {
 	# code...
 	$products = array();
 while($row=$exe->fetch_assoc()){
 	 $row["Source"];
 	 $row["Destination"];
+
+
 	
  array_push($products, $row);
 }

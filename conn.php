74d81457c35a63535aa2ea4a74437f2e2698dd04
anju-1312 <113588224+anju-1312@users.noreply.github.com>
2022-09-29 22:50:13 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -5,21 +5,18 @@
         "Uid" => "divya", // update me
         "PWD" => "Devesh-2002" // update me
     );
-    $conn = sqlsrv_connect($serverName, $connectionOptions);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    echo "Connected successfully";
+    
+    
 
-    //Establishes the connection
-    //$conn = sqlsrv_connect($serverName, $connectionOptions);
-    //$tsql= "SELECT TOP (1000) * FROM [dbo].[bus]";
-    //$getResults= sqlsrv_query($conn, $tsql);
-    //echo ("Reading data from table" . PHP_EOL);
-    //if ($getResults == FALSE)
-        //echo (sqlsrv_errors());
-    //while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-     //echo ($row['Name'] . " " . $row['Bno'] . PHP_EOL);
-    //}
-    //sqlsrv_free_stmt($getResults);
+    Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "SELECT TOP (1000) * FROM [dbo].[bus]";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['Name'] . " " . $row['Bno'] . PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults);
 ?>
